b79c1b7f120027512aab61c21357a9db3d65fe74
Christoph <christoph.weichselbaum@gmx.net>
2015-12-10 15:38:29 +0100

    templating

--- app/src/main/assets/web/main.js
+++ app/src/main/assets/web/main.js
@@ -1,19 +1,22 @@
 require.config({
-	paths: {
-		jquery: 'vendor/scripts/jquery',
-		jqueryMobile: 'vendor/scripts/jquery.mobile-1.3.2',
+    paths: {
+        jquery: 'vendor/scripts/jquery',
+        jqueryMobile: 'vendor/scripts/jquery.mobile-1.3.2',
         app: 'app',
         map: 'pages/map/controller/map.controller'
-	},
-  shim: {
-	"vendor/scripts/jquery.mobile-1.3.2'" : { deps: ["jquery"], exports: 'jquery' },
-  }
+    },
+    shim: {
+        "vendor/scripts/jquery.mobile-1.3.2'": {deps: ["jquery"], exports: 'jquery'},
+    }
 });
 
 require(["jquery",
-	"jqueryMobile",
+    "jqueryMobile",
     "app",
     "map"
 ], function ($) {
-  console.log('jQuery version ' + $().jquery + ' installed');
+    console.log('jQuery version ' + $().jquery + ' installed');
+    $(window).on('load', function () {
+        $("#index").show();
+    });
 });
